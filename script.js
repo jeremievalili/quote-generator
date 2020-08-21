d7dbbe29b9f989fdf91963c680fe43a448f3c1c1
Jeremie Madara <jeremiemadara@gmail.com>
2020-08-21 09:19:59 +0200

    Add Javascript

--- script.js
+++ script.js
@@ -1,16 +1,65 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+
+// Show Loading 
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading 
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 // Get quote from API 
 async function getQuote() {
+    loading()
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/' 
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data);
+        // If Author is blank, add 'Unknown'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown'
+        } else {
+            authorText.innerHTML = data.quoteAuthor;
+        }
+
+        // Reduce font size for long quotes
+        if (data.quoteText.length > 110 ) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote')
+        }
+        quoteText.innerHTML = data.quoteText;
+        // Stop Loader, Show Quote
+        complete();
+       
     } catch (error) {
         getQuote();
-        console.log('whoops, no quote', error)
     }
 }
 
+// Tweet a Quote 
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Button functions
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 // On Load
-getQuote();
+getQuote()
